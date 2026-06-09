fa1899ff728d4fbab7e01a058b4945ba5827d9a8
DRG <149902962+davidroguete05@users.noreply.github.com>
2026-06-09 10:02:29 -0400

    Add logic for a single round
    -Structure the logic for a single round without modifiying the scores yet
    -Adjust some indentation flaws to make the code more readable

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 // Algorithm rock paper scissors
 
 //Declare variables for both human and computer scores
+
 let humanScore = 0;
 let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 //1. Write a function to allow the pc to choose a random number between 1 and 3
 function getComputerChoice(max=3, min=1){
@@ -17,10 +20,27 @@ function getComputerChoice(max=3, min=1){
   }
 }
 
-    //2. Write a function to allow the user to choose among the three options rock paper or scissors
+//2. Write a function to allow the user to choose among the three options rock paper or scissors
 function getHumanChoice(){
   let humanChoice = prompt("rock, paper or scissors? ").toLowerCase(); //Make user input case-insensitive
   return humanChoice;
 }
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+
+//3.Write the logic for a single round
+//SET function playRound with two parameters humanChoice and computerChoice
+function playRound(humanChoice, computerChoice){
+  if(humanChoice === computerChoice){
+    console.log("It's a tie!");
+  } else if(humanChoice === "rock" && computerChoice === "scissors"){
+    console.log(`You won ${humanChoice} beats ${computerChoice}!`);
+  } else if(humanChoice === "paper" && computerChoice === "rock"){
+    console.log(`You won ${humanChoice} beats ${computerChoice}!`);
+  } else if(humanChoice === "scissors" && computerChoice === "paper"){
+    console.log(`You won ${humanChoice} beats ${computerChoice}!`);
+  } else {
+    console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
+  }
+}
+playRound(humanSelection, computerSelection);
+//playRound function will console.log a string representing the winner
+//Increment the humanScore or computerScore variable based on the round winner
