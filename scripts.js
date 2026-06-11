ae4421f93b3a5f6c22dcaa9094560ddfd8dc2466
DRG <149902962+davidroguete05@users.noreply.github.com>
2026-06-11 09:54:06 -0400

    Rework the way playRound function handle date
    -This was done to make the code more concice and return a value
    easier to manipulate for the upcoming playGame function
    
    Add some new pseudocode for the playGame function to test it

--- scripts.js
+++ scripts.js
@@ -29,18 +29,38 @@ function getHumanChoice(){
 //3.Write the logic for a single round
 //SET function playRound with two parameters humanChoice and computerChoice
 function playRound(humanChoice, computerChoice){
-  if(humanChoice === computerChoice){
-    console.log("It's a tie!");
-  } else if(humanChoice === "rock" && computerChoice === "scissors"){
-    console.log(`You won ${humanChoice} beats ${computerChoice}!`);
-  } else if(humanChoice === "paper" && computerChoice === "rock"){
-    console.log(`You won ${humanChoice} beats ${computerChoice}!`);
-  } else if(humanChoice === "scissors" && computerChoice === "paper"){
-    console.log(`You won ${humanChoice} beats ${computerChoice}!`);
-  } else {
-    console.log(`You lose ${computerChoice} beats ${humanChoice}!`);
-  }
+    return result = (humanChoice === computerChoice) ? "tie":
+    (humanChoice === "rock" && computerChoice === "scissors") ? "win":
+    (humanChoice === "paper" && computerChoice === "rock") ? "win":
+    (humanChoice === "scissors" && computerChoice === "paper") ? "win":
+    "lose";
 }
-playRound(humanSelection, computerSelection);
-//playRound function will console.log a string representing the winner
-//Increment the humanScore or computerScore variable based on the round winner
+
+//4. Write a function called playGame that execute, record scores and declare a winner
+/*INIT function playGame 
+Declare variable humanScore
+Declare variable computerScore
+Declare variable roundCounter
+CALL playRound
+Reduce roundCounter every round
+IF (roundCounter less than or equal to 0)
+  IF humanScore === computerScore
+    Show in the screen IT was a draw game
+  ELSE IF humanScore > computerScore
+    Show in the screen YOU won the game!
+  ELSE
+    Showw in the screen you lose the game
+ELSE
+  IF playRound is equal to "tie"
+    Do not update scores
+    alert technical tie
+    CALL playround
+  ELSE IF playRound is equal to "win"
+    Sum 1 point to humanScore
+    alert you won this round
+    CALL playRound
+  ELSE
+    Sum 1 point to computer score
+    alert you lose this option wins that option
+    CALL playRound
+*/ 
